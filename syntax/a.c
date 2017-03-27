ff302f1a064f6eec1b790596623fab9635efab1d
echo <3166239934@qq.com>
2017-03-27 17:29:20 +0800

    语法分析器

---
+++ syntax/a.c
@@ -0,0 +1,16 @@
+
+int f;
+int a ,v= 10;;
+float b = 20;
+char ch = 'a';
+string s = "hello", b = "sns";
+
+
+int f (int a, char b);
+int a(int a, int b);
+int main (int argc,string [] argv) 
+{
+    print(a);
+}
+
+
